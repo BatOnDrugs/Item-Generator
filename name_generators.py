d7ef9c8a68ca5b9919354e7df511bd950a05855c
BatOnDrugs <bartlomiej.gozdek@gmail.com>
2024-12-25 01:39:42 +0100

    added armour name generator and a list of armour name suffixes

--- name_generators.py
+++ name_generators.py
@@ -3,8 +3,16 @@ import random
 with open("weapon_names.md", "r") as content:
     weapon_names_list = content.read().split("\n")
 
+with open("armour_names.md", "r") as content:
+    armour_names_list = content.read().split("\n")
+
 def weapon_name_generator(weapon_type):
     weapon_name = random.choice(weapon_names_list)
     return f"{weapon_type} of {weapon_name}"
 
-print(weapon_name_generator("Sword"))
+def armour_name_generator(armour_piece):
+    armour_name = random.choice(armour_names_list)
+    return f"{armour_piece} of {armour_name}"
+
+print(armour_name_generator("Greaves"))
+
