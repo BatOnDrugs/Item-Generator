e53a90c8d60fa93d4b218dc61a48e3650cc4d100
BatOnDrugs <bartlomiej.gozdek@gmail.com>
2024-12-25 03:30:30 +0100

    changed singular stats into a list of stats for both armour and weapon

--- item_classes.py
+++ item_classes.py
@@ -1,23 +1,23 @@
 class Weapon():
-    def __init__(self, weapon_type, name, rarity, damage, attack_speed, range, weight, dmg_type, is_magical = False):
+    def __init__(self, weapon_type, name, rarity, weapon_stats_dict, dmg_type, is_magical = False):
         self.weapon_type = type
         self.rarity = rarity
-        self.dmg = damage
-        self.attack_speed = attack_speed
-        self.dps = damage * attack_speed
-        self.range = range
-        self.weight = weight
+        self.dmg = weapon_stats_dict[damage]
+        self.attack_speed = weapon_stats_dict[attack_speed]
+        self.dps = self.dmg * self.attack_speed
+        self.range = weapon_stats_dict[weapon_range]
+        self.weight = weapon_stats_dict[weight]
         self.dmg_type = dmg_type
         self.is_magical = is_magical
         self.special_power = ""
 
 class Armour():
-    def __init__(self, armour_piece, rarity, armour_class, defense, weight, is_magical = False):
+    def __init__(self, armour_piece, rarity, armour_class, armour_stats_dict, is_magical = False):
         self.armour_piece = armour_piece
         self.rarity = rarity
         self.armour_class = armour_class
-        self.defense = defense
-        self.weight = weight
+        self.defense = armour_stats_dict[defense]
+        self.weight = armour_stats_dict[weight]
         self.is_magical = False
         self.special_power = ""
 
