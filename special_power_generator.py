97116164dd53fd61a8cdc7fb9cdeb7366618fe4a
BatOnDrugs <bartlomiej.gozdek@gmail.com>
2024-12-25 01:16:53 +0100

    added special power pickers

---
+++ special_power_generator.py
@@ -0,0 +1,18 @@
+import random
+
+with open("weapon_effects.md", "r") as content:
+    weapon_effects_list = content.read().split("\n")
+
+with open("armour_effects.md", "r") as content:
+    armour_effects_list = content.read().split("\n")
+
+def weapon_power_picker():
+    return random.choice(weapon_effects_list)
+
+
+
+def armour_effect_picker():
+    return random.choice(armour_effects_list)
+
+print(armour_effect_picker()) 
+
