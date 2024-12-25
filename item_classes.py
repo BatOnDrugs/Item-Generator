e35d7fcc93e032cd82fc591384266d4d52e7dc5f
BatOnDrugs <bartlomiej.gozdek@gmail.com>
2024-12-25 01:29:27 +0100

    added type to weapons and armour piece to armour

--- item_classes.py
+++ item_classes.py
@@ -1,5 +1,6 @@
 class Weapon():
-    def __init__(self, name, rarity, damage, attack_speed, dps, weight, dmg_type, is_magical = False):
+    def __init__(self, type, name, rarity, damage, attack_speed, dps, weight, dmg_type, is_magical = False):
+        self.type = type
         self.rarity = rarity
         self.dmg = damage
         self.attack_speed = attack_speed
@@ -7,12 +8,17 @@ class Weapon():
         self.weight = weight
         self.dmg_type = dmg_type
         self.is_magical = is_magical
+        self.special_power = ""
 
 class Armour():
-    def __init__(self, rarity, armour_class, defense, weight, is_magical = False):
+    def __init__(self, armour_piece, rarity, armour_class, defense, weight, is_magical = False):
+        self.armour_piece = armour_piece
         self.rarity = rarity
         self.armour_class = armour_class
         self.defense = defense
         self.weight = weight
         self.is_magical = False
+        self.special_power = ""
+
+
 
