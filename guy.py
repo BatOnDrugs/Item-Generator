d2db1364abe3c6bf6614a24ab086f48b55f1f0f3
BatOnDrugs <bartlomiej.gozdek@gmail.com>
2024-12-27 05:31:43 +0100

    added attack and get_hit methods

--- guy.py
+++ guy.py
@@ -2,7 +2,8 @@ from inventory import Inventory
 from item_classes import Weapon, Armour
 
 class Guy():
-    def __init__(self, health, strength):
+    def __init__(self, health, strength, name):
+        self.name = name
         self.health = health
         self.strength = strength
         self.inventory = Inventory(self.strength)
@@ -15,17 +16,31 @@ class Guy():
         self.inventory.equip_all_possible_items()
         print(self.inventory.check_equipped_items())
         self.modify_health()
+        print(f"Current Health is {self.health}")
+        print(f"Current strength is {self.strength}")
         
         
 
     def modify_health(self):
         for item in self.inventory._used_slots:
             if isinstance(self.inventory.slots[item], Weapon):
-                self.strength += self.inventory.slots[item].dmg
+                self.strength += int(self.inventory.slots[item].dmg)
             elif isinstance(self.inventory.slots[item], Armour):
-                self.health += self.inventory.slots[item].defense
+                self.health += int(self.inventory.slots[item].defense)
+
+    def attack(self, other):
+        print(f"{self.name} hit {other.name} for {self.strength} damage!")
+        return other.get_hit(self)
+    
+    def get_hit(self, other):
+        print(f"{self.name} got hit by {other.name} for {other.strength} damage!")
+        self.health -= other.strength / 2
+        print(f"{self.name} has {self.health} health left!")
         
         
 
-guy1 = Guy(100, 100)
-print(guy1.prepare_for_battle(5))
+guy1 = Guy(100, 10, "Jacob")
+guy2 = Guy(100, 10, "Edward") 
+guy1.prepare_for_battle(20)
+guy2.prepare_for_battle(20)
+guy1.attack(guy2)
